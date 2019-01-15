9c99cfd368972b3ff89b1b6a19528974f1046c00
Tom Saleeba <tom.saleeba@gmail.com>
2019-01-15 12:55:59 +1030

    fix: attempt to get editing of new pages working

--- src/js/cms.js
+++ src/js/cms.js
@@ -2,9 +2,8 @@ import React from "react";
 import CMS from "netlify-cms";
 
 import HomePreview from "./cms-preview-templates/home";
-import PostPreview from "./cms-preview-templates/post";
-import ProductsPreview from "./cms-preview-templates/products";
-import ValuesPreview from "./cms-preview-templates/values";
+import TestimonialsPreview from "./cms-preview-templates/testimonials";
+import GalleryPreview from "./cms-preview-templates/gallery";
 import ContactPreview from "./cms-preview-templates/contact";
 
 
@@ -22,8 +21,7 @@ class ColorControl extends React.Component {
 
 CMS.registerPreviewStyle("/css/main.css");
 CMS.registerPreviewTemplate("home", HomePreview);
-CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("products", ProductsPreview);
-CMS.registerPreviewTemplate("values", ValuesPreview);
+CMS.registerPreviewTemplate("testimonials", TestimonialsPreview);
+CMS.registerPreviewTemplate("gallery", GalleryPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
 CMS.registerWidget("color", ColorControl);
